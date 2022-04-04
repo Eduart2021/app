5811909e977c88e8e8bb057ad7996639da9d4e0a
Eduart2021 <zeedymarina@yahoo.com>
2022-04-04 00:19:22 -0400

    react

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const items = require('./routes/api/items')
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -12,7 +14,7 @@ mongoose.connect(db)
 .then(()=>{console.log("Server conected")})
 .catch(err => {console.log(err)})
 
-
+app.use('/api/items', items)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
